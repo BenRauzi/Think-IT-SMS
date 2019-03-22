410455079de61eb4671d0d49f53fb254be734f82
Ben Rauzi <ben.rauzi@outlook.com>
2019-03-23 10:08:53 +1300

    Notices Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent} from './dashboard/dashboard.component';
+import { NoticesComponent} from './notices/notices.component';
 import { RegisterComponent } from './register/register.component';
 import { AdminOnlyTestComponent } from './admin-only-test/admin-only-test.component';
 import { AuthGuard, RoleGuard } from '../guards';
+
 
 const routes: Routes = [
   { path:'login', component:LoginComponent },
   { path:'dashboard', component: DashboardComponent, canActivate: [ AuthGuard ] },
   { path:'register', component: RegisterComponent },
+  { path:'notices', component: NoticesComponent },
   { path: 'adminonly', component: AdminOnlyTestComponent, data: { role: "Administrator" }, canActivate: [ RoleGuard, AuthGuard ] },
   { path:'', redirectTo:'login', pathMatch:"full" },
   { path:'**', component:LoginComponent }
