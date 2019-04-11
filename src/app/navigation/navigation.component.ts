d656fb139cd3e4f2385417661276336abb141951
Ben Rauzi <ben.rauzi@outlook.com>
2019-04-11 16:48:22 +1200

    Icons and Styling - @Isaac
    @Isaac

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -54,7 +54,7 @@ export class NavigationComponent implements OnInit {
       this.closeNav()
       return
     }
-    const menuItems = ["teacher", "teacher_notices", "asd","Oof"]
+    const menuItems = ["teacher", "teacher_notices", "results","ncea_summary"]
     document.getElementById("sideNav").style.width = "10%";
 
     [].forEach.call(document.querySelectorAll('.other'), function (el) {
