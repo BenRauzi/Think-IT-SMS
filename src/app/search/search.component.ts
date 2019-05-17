67be5b47dedd3ae6d4272ba258190bc8dbd63799
Isaac <Isaactimothylyne@gmail.com>
2019-05-17 12:24:09 +1200

    Navigation Function added!
    Search Navigation added support for dashboard

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,5 +11,14 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
+  closeNav() {
+    if ( document.getElementById("sideNav").style.width != "") {
+      document.getElementById("sideNav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0%";
+      try {
+        document.getElementById("ribbon").style.marginLeft = "0%";
+      } catch(err) {}
+    }
+  }
 }
