65daa63d60679ca3d9022c8134ba657e0ee455d9
Isaac <Isaactimothylyne@gmail.com>
2019-06-05 08:57:08 +1200

    Search pagebuttons

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,17 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef, ViewChild} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
 
-  constructor() { 
-  }
+export class SearchComponent implements OnInit {
+  constructor(private _snackBar: MatSnackBar) {}
+
   ngOnInit() {
   }
   
@@ -24,5 +25,44 @@ export class SearchComponent implements OnInit {
       } catch(err) {}
     }
   }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+  yearDialog():void{
+
+  }
+}
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  curclass: string;
 }
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, curclass: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, curclass: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, curclass: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, curclass: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, curclass: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, curclass: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, curclass: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, curclass: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, curclass: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, curclass: 'Ne'},
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
+@Component({
+  selector: 'table-basic-example',
+  styleUrls: ['table-basic-example.css'],
+  templateUrl: 'table-basic-example.html',
+})
+export class TableBasicExample {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'curclass'];
+  dataSource = ELEMENT_DATA;
+}
